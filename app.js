d8820d3f36335aeacbb687ce53f79b83d2eec642
Yustynn Panicker <yustynn@gmail.com>
2016-09-25 00:20:09 +0800

    Fix bug where dl link shows up when it shouldnt, eg website links

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const DL_ICON_SRC = 'https://images.designtrends.com/wp-content/uploads/2015/12/02045154/Download-Icons38.png'
 const EDIM_URL = 'https://edimension.sutd.edu.sg';
 
-const links = $('#content_listContainer li a');
+let links = $('#content_listContainer li a');
+
+console.log('before', links);
+
+links = links.filter(function() {
+  const imgSrc = $(this).closest('li').children('img').attr('src');
+  console.log($(this).closest('li').children('img'))
+
+  return imgSrc.includes('document') || imgSrc.includes('file');
+});
+
+console.log('after', links);
 
 links.each(function(i) {
   const url = EDIM_URL + $(this).attr('href');
