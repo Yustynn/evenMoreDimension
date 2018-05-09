645cd8cbd600f6fdff241586917f71aafd6a0fe7
Kevan <kevan.tan@live.com>
2018-05-09 11:12:58 +0800

    feat(download-subdirectories): got the download folder buttons to have nested structure

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,20 @@
-chrome.runtime.onMessage.addListener((url) => {
-  chrome.downloads.download({ url });
+chrome.runtime.onMessage.addListener(({url, curr_path, title}) => {
+  // const filename = `${curr_path}/${title}`;
+  // filenam
+  // console.log(filename)
+  title = title.substring(1);
+  if (!title) {
+    return true;
+  }
+  //curr_path = curr_path.replace(/[\\\:\*\]\?\"\<\>\|]/g, '');
+  // title = title.split(' ').join('\ ');
+  // curr_path = curr_path.split(' ').join('\ ');
+  const filename = `${curr_path}/${title}`;
+  console.log(filename);
+  chrome.downloads.download({ url, filename });
 })
+
+// chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+//   console.log(item);
+//   suggest({filename: 'edim/' + item.filename});
+// });
