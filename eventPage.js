de4580b06b1b8d653b5268da181458cd2ed9a8b9
Kevan <kevan.tan@live.com>
2018-05-09 14:55:17 +0800

    feat(download-subdirectories): cleaned up console logs and comments

--- eventPage.js
+++ eventPage.js
@@ -1,22 +1,9 @@
 chrome.runtime.onMessage.addListener(({url, curr_path, title}) => {
-  // const filename = `${curr_path}/${title}`;
-  // filenam
-  // console.log(filename)
-  console.log(title);
-  title = (title[0] == '\xa0') ? title.substring(1) : title;
-  // title = title ? title.substring(1) : title;
-  if (!title) {
-    return true;
-  }
-  //curr_path = curr_path.replace(/[\\\:\*\]\?\"\<\>\|]/g, '');
-  // title = title.split(' ').join('\ ');
-  // curr_path = curr_path.split(' ').join('\ ');
+  // k. terminate sendMessage if title is empty ie. tabAction links
+  if (!title) { return true;}
+  
+  title = (title[0] == '\xa0') ? title.substring(1) : title; // k. check for &nbsp
   const filename = `${curr_path}${title}`;
-  //console.log(filename);
+
   chrome.downloads.download({ url, filename });
 })
-
-// chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
-//   console.log(item);
-//   suggest({filename: 'edim/' + item.filename});
-// });
